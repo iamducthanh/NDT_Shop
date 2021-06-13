b9c16f3c098543208f7a7ec080a221db6eb83386
iamducthanh <ducthanh260801@gmail.com>
2021-06-13 09:40:55 +0700

    dd

--- src/api/axiosClient.js
+++ src/api/axiosClient.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
 export const axiosClient = axios.create({
-    // baseURL: 'https://yc8vw.sse.codesandbox.io/',
-    baseURL: 'http://localhost:3005/',
+    baseURL: 'https://yc8vw.sse.codesandbox.io/',
+    // baseURL: 'http://localhost:3005/',
 
     headers: {
         'Content-Type': 'application/json',
